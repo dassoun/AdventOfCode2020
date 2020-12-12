3425e9008f5b76e28a7588cfa6ab2d0916dc0692
jcoignet <jcoignet@DES-7BGJRH2.office.coyotesystems.com>
2020-12-12 09:25:52 +0100

    Day 11 a

---
+++ src/Day11a.java
@@ -0,0 +1,101 @@
+import java.util.ArrayList;
+
+import com.jco.utils.FileTransfom;
+import com.jco.utils.StringUtils;
+
+public class Day11a {
+
+	public static void main(String[] args) {
+		String fileName = "inputs/day11a.txt";
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list = FileTransfom.fileToStringList(fileName);
+		
+		printLayout(list);
+
+		int length = list.get(0).length();
+		
+		String tmp = StringUtils.padLeft('.', "", length + 2);
+		list.add(0, tmp);
+		for (int i = 1; i < list.size(); i++) {
+			list.set(i, "." + list.get(i) + ".");
+		}
+		list.add(tmp);
+		
+		ArrayList<String> newLayout = (ArrayList<String>) list.clone();
+		
+		int nbMove = 0;
+			
+		do {
+			nbMove = 0;
+			
+			for (int i = 1; i < list.size() - 1; i++) {
+				for (int j = 1; j < list.get(0).length() - 1; j++) {
+					if (list.get(i).charAt(j) == 'L') {
+						int nbOccupied = 0;
+						for (int k = -1; k <= 1; k++) {
+							for (int l = -1; l <= 1; l++) {
+								if (k != 0 || l != 0) {
+									if (list.get(i + k).charAt(j + l) == '#') {
+										nbOccupied++;
+									}
+								}
+							}
+						}
+						if (nbOccupied == 0) {
+							newLayout.set(i, (StringUtils.replaceCharAt(newLayout.get(i), j, '#')));
+							nbMove++;
+						}
+					} else if (list.get(i).charAt(j) == '#') {
+						int nbOccupied = 0;
+						for (int k = -1; k <= 1; k++) {
+							for (int l = -1; l <= 1; l++) {
+								if (k != 0 || l != 0) {
+									if (list.get(i + k).charAt(j + l) == '#') {
+										nbOccupied++;
+									}
+								}
+							}
+						}
+						if (nbOccupied >= 4) {
+							newLayout.set(i, (StringUtils.replaceCharAt(newLayout.get(i), j, 'L')));
+							nbMove++;
+						}
+//						System.out.println(list.get(i));
+//						System.out.println(newLayout.get(i));
+					}
+				}
+			}
+			
+			list = (ArrayList<String>) newLayout.clone();
+			
+			printLayout(list);			
+			
+		} while (nbMove > 0);
+		
+		printLayout(list);
+		
+		System.out.println();
+	}
+	
+	public static void printLayout(ArrayList<String> list) {
+		for (String l : list) {
+			System.out.println(l);
+		}
+		System.out.println(countChar(list, '#'));
+	}
+
+	public static int countChar(ArrayList<String> list, char c) {
+		int count = 0;
+		
+		for (int i = 1; i < list.size() - 1; i++) {
+			for (int j = 1; j < list.get(0).length() - 1; j++) {
+				if (list.get(i).charAt(j) == c) {
+					count++;
+				}
+			}
+		}
+		
+		return count;
+	}
+}
